553ddf740f46ced0d781a8029f711f8ee0bd8af4
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2023-03-26 11:16:56 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# MomoDB
+
+A small in memory database for storing and retrieving data, built in Rust. Currently, this is in development, and should not be used in production.
+
+## Future
+
+- [ ] Add support for python
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use crate::datastore::datastore::DataStore;
+
+fn main() {
+    let mut store = DataStore::new();
+
+    // Store a value
+    store.put("foo", "bar");
+    let value = store.get("foo").unwrap();
+    println!("Value of foo: {}", value);
+    store.delete("foo");
+
+    // This will fail because the value is no longer in the store
+    println!("Value of foo: {:?}", store.get("foo"));
+}
